b90605982a7fef991c7d00ed7c2beb46d807e1b3
Shane Haw <10742171+shanehaw@users.noreply.github.com>
2018-07-13 14:56:54 +0200

    expand first test for create basic equation

--- src/test/kotlin/EquationMaker/EqMakerTest.kt
+++ src/test/kotlin/EquationMaker/EqMakerTest.kt
@@ -6,15 +6,30 @@ import EquationCore.Parser
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Test
+import kotlin.test.assertTrue
 
 
 class EqMakerTest {
 
+    fun createEqMaker() : EqMaker {
+        return EqMaker() //EqMaker(Eq(Parser(Scanner())))
+    }
+
     @Test
     fun `can create basic equation`() {
-        val creationString = "[0-9] + 1 = [1-10]"
-        val eqMaker = EqMaker() //EqMaker(Eq(Parser(Scanner())))
+        val creationString = "[0-9] + 1 = {1-10}"
+        val eqMaker = createEqMaker()
         val possibilities: List<String> = eqMaker.parse(creationString)
-        assertThat(possibilities.count(), `is`(9))
+        assertTrue { possibilities.count() == 9}
+        assertTrue { possibilities.contains("0 + 1 = ") }
+        assertTrue { possibilities.contains("1 + 1 = ") }
+        assertTrue { possibilities.contains("2 + 1 = ") }
+        assertTrue { possibilities.contains("3 + 1 = ") }
+        assertTrue { possibilities.contains("4 + 1 = ") }
+        assertTrue { possibilities.contains("5 + 1 = ") }
+        assertTrue { possibilities.contains("6 + 1 = ") }
+        assertTrue { possibilities.contains("7 + 1 = ") }
+        assertTrue { possibilities.contains("8 + 1 = ") }
+        assertTrue { possibilities.contains("9 + 1 = ") }
     }
 }
